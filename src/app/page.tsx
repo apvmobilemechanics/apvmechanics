76a7b5a2531c325fab4d44045265591408001339
wizora-studio <abdullah1aa3063358@gmail.com>
2026-08-06 23:45:46 +0500

    style: enhance process section mobile responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -264,7 +264,7 @@ function Contact() {
           <p>Book your repair or maintenance visit today. Our friendly specialists are equipped with professional diagnostic &amp; repair tools.</p>
           <div><i><Phone/></i><span><small>Call Anytime</small><b><a href="tel:0424411375" style={{ color: "inherit" }}>0424 411 375</a></b></span></div>
           <div><i><Mail/></i><span><small>Email Address</small><b><a href="mailto:apvmobilemechanics@gmail.com" style={{ color: "inherit" }}>apvmobilemechanics@gmail.com</a></b></span></div>
-          <div><i><MapPin/></i><span><small>Our Location</small><b><a href="https://www.google.com/maps/place/APV+mobile+Mechanics/@-42.7871385,147.2448997,17z/data=!3m1!4b1!4m6!3m5!1s0xaa6e0d2733b06751:0xe9d97dff7c553e69!8m2!3d-42.7871425!4d147.24748!16s%2Fg%2F11z96__8np!18m1!1e1?entry=ttu&g_ep=EgoyMDI2MDgwMy4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer" style={{ color: "inherit", textDecoration: "underline" }}>APV Mobile Mechanics</a></b></span></div>
+          <div><i><MapPin/></i><span><small>Get Directions</small><b><a href="https://www.google.com/maps/place/APV+mobile+Mechanics/@-42.7871385,147.2448997,17z/data=!3m1!4b1!4m6!3m5!1s0xaa6e0d2733b06751:0xe9d97dff7c553e69!8m2!3d-42.7871425!4d147.24748!16s%2Fg%2F11z96__8np!18m1!1e1?entry=ttu&g_ep=EgoyMDI2MDgwMy4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer" style={{ color: "inherit", textDecoration: "underline" }}>APV Mobile Mechanics</a></b></span></div>
 
           <div className="contact-tools-grid">
             <h4 className="contact-tools-heading">Mobile Tools &amp; Diagnostic Equipment</h4>
